128eeedd6a636632008ac18f83282fa91193cf3f
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2016-10-10 13:18:41 +0200

    Add help text

--- bot.js
+++ bot.js
@@ -5,6 +5,8 @@ const telegramTemplate = botBuilder.telegramTemplate
 const excuse = require('huh')
 const confData = require('./conf-data.json')
 
+Array.prototype.random = function () { return this[Math.floor((Math.random()*this.length))] }
+
 module.exports =  botBuilder(message => {
   if (message.text === '/start')
     return [
@@ -21,12 +23,10 @@ module.exports =  botBuilder(message => {
 
   function splitIntoChunks(arr, chunkSize) {
     const newArr = []
-
     while (arr.length > 0) {
       let chunk = arr.splice(0, chunkSize)
       newArr.push(chunk)
     }
-
     return newArr
   }
 
@@ -96,30 +96,15 @@ module.exports =  botBuilder(message => {
     return [
       new telegramTemplate.Text(`*HolyJS*\n\nDecember 11, Moscow\nRadisson Slavyanskaya Hotel, Square of Europe, 2`).get(),
       new telegramTemplate.Text(`There is plenty of frontend conferences held in Russia. However, before this year there weren’t any conferences on the most popular in the world (according to GitHub and RedMonk) programming language, JavaScript, which is mainly associated with frontend.\n\nWe fixed this bug, and now we have HolyJS Moscow, which is the second large-scale conference on JavaScript in 2016. More than 400 JS-developers will come together to discuss questions with JS-experts from all over the world.\n\nIt is guaranteed that all the talks will be on technical topics without any agile, scrum and team management stuff.`)
-        .addInlineKeyboard([
-          [{
-            text: 'HolyJS on facebook',
-            url: 'https://www.facebook.com/holyjs/'
-          }],
-          [{
-            text: 'HolyJS on VKontakte',
-            url: 'http://vk.com/holyjs'
-          }],
-          [{
-            text: '@HolyJSconf on twitter',
-            url: 'https://twitter.com/HolyJSconf'
-          }]
-        ])
+        .addInlineKeyboard([[{ text: 'HolyJS on facebook', url: 'https://www.facebook.com/holyjs/' }], [{ text: 'HolyJS on VKontakte', url: 'http://vk.com/holyjs' }], [{ text: '@HolyJSconf on twitter', url: 'https://twitter.com/HolyJSconf' }]])
         .get(),
       new telegramTemplate.Text(`The conference will include more than 20 technical talks spoken in parallel tracks, lots of new people and communication with colleagues. HolyJS is not only about frontend, it also touches backend, desktop, and other demanded topics of JavaScript world.`)
         .addReplyKeyboard([['List of topics'], ['Website'], ['Location'], ['Help'], ['Back to the main menu']], true, true)
         .get()
     ]
 
-  if (/^Follow @([0-1a-zA-Z_]{1,50}) on twitter$/i.test(message.text.toLowerCase())) {
-    console.log('Twitter', message.text.toLowerCase().replace(/^Follow @([0-1a-zA-Z_]{1,50}) on twitter$/i, 'https://twitter.com/$1'))
+  if (/^Follow @([0-1a-zA-Z_]{1,50}) on twitter$/i.test(message.text.toLowerCase()))
     return new telegramTemplate.Text(message.text.toLowerCase().replace(/^Follow @([0-1a-zA-Z_]{1,50}) on twitter$/i, 'https://twitter.com/$1')).get()
-  }
 
   if (/^About ([0-1a-zA-Z -]{1,100})$/i.test(message.text.toLowerCase())) {
     let name = message.text.toLowerCase().replace(/^About ([0-1a-zA-Z -]{1,100})$/i, '$1')
@@ -161,17 +146,18 @@ module.exports =  botBuilder(message => {
 
   if (message.text.toLowerCase() === 'help'  || message.text === '/help')
     return [
-      new telegramTemplate.Text(`Help text...`)
-        .addReplyKeyboard([['Schedule'], ['Current talk info'], ['Next talk info'], ['Info'], ['Help']], true, true)
+      new telegramTemplate.Text(['Здравствуйте!', 'Привет.', 'Как поживаешь?', 'Рад тебя видеть.', 'Как дела?'].random() + `\nI am *Igor*, your conference chat bot!\n\nI can help you with a few different things, including the schedule, talks and speakers info, info about the conference, etc. Simply follow the buttons on the keyboard or try to type. Keep in mind I am not that smart yet :) But you can help! Visit my Github repository and send PR to add or improve my skills:`)
+        .addInlineKeyboard([[{ text: 'Github repository', url: 'https://github.com/stojanovic/conference-telegram-bot' }]])
+        .get(),
+      new telegramTemplate.Text(`Here's a few things I can help you with`)
+        .addReplyKeyboard([['Schedule'], ['Current talk info'], ['Next talk info'], ['Info']], true, true)
         .get()
     ]
 
   if (message.text.toLowerCase() === 'back to the main menu'  || message.text === 'main menu')
-    return [
-      new telegramTemplate.Text(`Here's a few things that I can help you with`)
-        .addReplyKeyboard([['Schedule'], ['Current talk info'], ['Next talk info'], ['Info'], ['Help']], true, true)
-        .get()
-    ]
+    return new telegramTemplate.Text(`Here's a few things that I can help you with`)
+      .addReplyKeyboard([['Schedule'], ['Current talk info'], ['Next talk info'], ['Info'], ['Help']], true, true)
+      .get()
 
   return [
     new telegramTemplate.Text(`Hmm, I can't answer that 🙁 \n*Reason*:\n${excuse.get()}`).get(),
