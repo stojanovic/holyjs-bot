b8b9d26939f3bfa2b3698b4e9a8f1fc268dd883a
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2016-11-27 14:43:33 +0100

    Translate into Russian

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const botBuilder = require('claudia-bot-builder')
 const botFlow = require('./lib/bot-flow')
 
 function getLanguage() {
-  return new Promise(resolve => resolve('en'))
+  return new Promise(resolve => resolve('ru'))
 }
 
 module.exports = botBuilder(message => {
