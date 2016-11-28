f81833a0098b565a8dd11f57e437f7dd27f7df94
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2016-11-28 10:43:09 +0100

    Add language switcher and various updates

--- bot.js
+++ bot.js
@@ -4,15 +4,52 @@ const botBuilder = require('claudia-bot-builder')
 
 const botFlow = require('./lib/bot-flow')
 
-function getLanguage() {
-  return new Promise(resolve => resolve('ru'))
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+function getLanguage(id, table) {
+  let params = {
+    TableName: table,
+    Key: {
+      userId: id
+    }
+  }
+
+  console.log('params', params)
+
+  return docClient.get(params).promise()
+    .then(result => {
+      console.log('OK', result)
+      if (!result.Item)
+        throw new Error('Not found')
+      return result.Item.lang
+    })
+    .catch(err => {
+      params = {
+        TableName: table,
+        Item: {
+          userId: id,
+          lang: 'en'
+        }
+      }
+      console.log('Err', err)
+      return docClient.put(params).promise()
+        .then(res => {
+          console.log('Create', res)
+          return 'en'
+        })
+    })
 }
 
-module.exports = botBuilder(message => {
-  return getLanguage()
+const api = botBuilder((message, originalApiRequest) => {
+  return getLanguage(message.sender, originalApiRequest.env.dynamoDbTable)
     .then(lang => {
-      return botFlow(message, lang)
+      return botFlow(message, lang, originalApiRequest.env, docClient)
     })
 }, {
   platforms: ['telegram']
 })
+
+api.addPostDeployConfig('dynamoDbTable', 'DynamoDB table name:', 'configure-bot')
+
+module.exports = api
